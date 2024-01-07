5bdb4c14849dec91ef6104fa160230d46110a2d2
myaeyh <132278931+myaeyh@users.noreply.github.com>
2024-01-07 23:24:30 +0800

    確定到底github連結能不能用
    (待會更新sketch frag黨就好

---
+++ May.frag
@@ -0,0 +1,320 @@
+//一開始沒有塗 先看作品介紹
+//texture20 or play
+//picture不便
+//x-axis=0 Y axis=0.05
+//還沒cursor時就是沒有RGB色板
+//ink 000 background
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+vec2 uv;
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+uniform float u_density_texture;
+uniform float u_density_picture;
+
+uniform float u_speed_x;
+uniform float u_speed_y;
+uniform bool u_still;
+uniform bool u_mouse_pressed;
+
+uniform vec3 u_rgb;
+
+uniform vec3 u_color_ink;
+uniform vec3 u_color_background;
+
+uniform sampler2D u_texture_base;
+uniform sampler2D u_texture_0;
+uniform sampler2D u_texture_1;
+uniform sampler2D u_texture_2;
+uniform sampler2D u_texture_3;
+uniform sampler2D u_texture_4;
+uniform sampler2D u_texture_5;
+
+uniform sampler2D u_title;//左下那個大字還沒cursor之前
+uniform sampler2D u_guide;//左哨角得
+
+uniform sampler2D u_intro_background;//作品說明
+uniform sampler2D u_intro_title;
+uniform sampler2D u_intro_content;
+
+#define PI 3.1415926535897932384626433832795
+
+// -------------------- hash -------------------- //
+//生成擾動數值的 hash 函數->生成噪聲&複雜紋理的時候使用<-隨機性
+//使用 sin 函數生成一個週期性的值，並乘以一個大數值（40000.0）以放大影響
+//fract 函數用於取得 sin(n) * 40000.0 的小數部分，將結果限制在 [0, 1] 的範圍內
+float hash(in float n)
+{
+    return fract(sin(n) * 40000.0);
+}
+
+// -------------------- random2 -------------------- //
+//目的：在二維空間中生成擾動的向量->常用於創建隨機模式或引入變動，ex生成噪聲紋理或其他複雜的圖形
+//CODE：使用 sin 和 dot 函數生成兩個擾動值，每個擾動值都是通過 dot 乘積計算的
+    // 使用不同常數向量（vec2(127.1, 311.7) 和 vec2(269.5, 183.3)）區分不同的擾動
+    // 將這兩個擾動值傳入 sin 函數中，並乘以一個大數值（43758.5453）
+    // 最後使用 fract 函數取得 sin(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3))) * 43758.5453 的小數部分->[0,1]
+vec2 random2(vec2 p)
+{
+    return fract(sin(vec2(dot(p,vec2(127.1, 311.7)),
+                                   dot(p,vec2(269.5, 183.3)))) * 43758.5453);
+}
+
+// -------------------- voronoi -------------------- //
+//會用到的程式碼被註解掉了
+
+vec3 voronoi(in vec2 x)
+{
+    vec2 n = floor(x);
+    vec2 f = fract(x);
+
+    vec2 mg, mr;
+    float md = 8.0;
+    for (int j = -1; j <= 1; j++)
+    {
+        for (int i = -1; i <= 1; i++)
+        {
+            vec2 g = vec2(float(i), float(j));
+            vec2 o = random2(n + g);
+            o = 0.5 + 0.5 * sin(u_time + 6.2831 * o);
+
+            vec2 r = g + o - f;
+            float d = dot(r,r);
+
+            if( d < md ) {
+                md = d;
+                mr = r;
+                mg = g;
+            }
+        }
+    }
+
+    md = 8.0;
+    for (int j = -2; j <= 2; j++)
+    {
+        for (int i = -2; i <= 2; i++)
+        {
+            vec2 g = mg + vec2(float(i), float(j));
+            vec2 o = random2(n + g);
+            o = 0.5 + 0.5 * sin(u_time + 6.2831 * o);
+
+            vec2 r = g + o - f;
+
+            if (dot(mr-r, mr-r) > 0.00001)
+            {
+                md = min(md, dot(0.5 * (mr + r), normalize(r - mr)));
+            }
+        }
+    }
+    return vec3(md, mr);
+}
+
+// -------------------- colorTransform -------------------- //
+
+vec3 colorTransform(vec3 color)
+{
+    color.xyz /= 255.0;//改這裡滑鼠沒差
+    return color;
+}
+
+// -------------------- mouseEffect -------------------- //
+
+// float mouseEffect(vec2 uv, vec2 mouse, float size)
+// {
+//     float dist = length(uv - mouse);
+//     return smoothstep(size, size, dist);
+// }
+
+float breathing=(exp(sin(u_time*2.0*3.14159/8.0)) - 0.36787944)*0.42545906412;
+float mouseEffect(vec2 uv, vec2 mouse, float size)
+{
+    float dist=length(uv-mouse);
+    return smoothstep(size, size+0.2*(breathing+0.5), dist);  //size
+}
+
+// -------------------- main -------------------- //
+
+void main()
+{
+    uv = gl_FragCoord.xy / u_resolution.xy;
+    uv.x *= u_resolution.x / u_resolution.y;
+    //寬高比???這樣的調整確保了在不同寬高比的屏幕上，UV坐標的X分量被縮放，使得紋理或效果在視覺上看起來正確，不會因為寬高比的變化而變形
+
+    vec2 uv_mouse = u_mouse / u_resolution.xy; //通過除以屏幕的分辨率( u_resolution.xy包含屏幕分辨率的統一變量)，將滑鼠位置轉換為規範化的坐標系，即在 [0, 1] 的範圍內。
+    uv_mouse.x *= u_resolution.x / u_resolution.y;
+
+    float highlight_size = 0.1;//滑鼠高光筆
+    float texture_mouse = mouseEffect(uv, uv_mouse, highlight_size*-sin(u_time));//因為光暈的跟呼吸效果可能會打架
+
+//u_title是從js設定字體的左下角的自，在texturetitle的地方使用到u_title
+    vec2 uv_title = vec2(uv.x , uv.y );//存儲新的紋理座標。在原始的紋理座標基礎上，向右偏移了 0.45，向上偏移了 0.45。這可能是為了定位紋理中的某個區域，使其在視覺上更好地顯示在屏幕上的特定位置。
+    uv_title.y = 1.0 - uv_title.y;//因為店腦座標的方向相反??
+	vec4 texture_title = texture2D(u_title, uv_title);
+//u_guide是左上的cursor
+    vec2 uv_guide = vec2(uv.x + 0.45, uv.y - 0.45);//是否是他最低就是0.45
+    uv_guide.y = 1.0 - uv_guide.y;
+	vec4 texture_guide = texture2D(u_guide, uv_guide);
+
+    vec2 uv_intro_background = uv;
+    vec4 texture_intro_background = texture2D(u_intro_background, uv_intro_background);
+
+    vec2 uv_intro_title = vec2(uv.x + 0.45, uv.y + 0.45);
+    uv_intro_title.y = 1.0 - uv_intro_title.y;
+	vec4 texture_intro_title = texture2D(u_intro_title, uv_intro_title);
+
+    vec2 uv_intro_content = vec2(uv.x + 0.45, uv.y - 0.45);
+    uv_intro_content.y = 1.0 - uv_intro_content.y;
+	vec4 texture_intro_content = texture2D(u_intro_content, uv_intro_content);
+
+    if (u_still) { uv = uv; }//是否移動
+    else
+    {
+        uv.x -= u_time * u_speed_x;
+        uv.y -= u_time * u_speed_y;
+    }
+
+    vec4 graphic;
+
+    // -------------------------------------------------- //
+
+    // vec2 uv_graphic = uv * 3.0;
+    // vec3 graphic_voronoi = voronoi(uv_graphic);
+
+    // graphic.rgb = graphic_voronoi.x * (0.5 + 0.5 * sin(64.0 * graphic_voronoi.x)) * vec3(1.0);
+    // graphic.rgb = mix(vec3(1.0),
+    //                   graphic.rgb,
+    //                   smoothstep(0.1, 0.02, graphic_voronoi.x));
+
+    // -------------------------------------------------- //
+
+    // vec2 uv_graphic = uv * 6.0;
+
+    // vec2 i_st = floor(uv_graphic);
+    // vec2 f_st = fract(uv_graphic);
+
+    // float m_dist = 1.;
+    // for (int j = -1; j <= 1; j++)
+    // {
+    //     for (int i = -1; i <= 1; i++)
+    //     {
+    //         vec2 neighbor = vec2(float(i), float(j));
+
+    //         vec2 offset = random2(i_st + neighbor);
+    //         offset = 0.5 + 0.5 * sin(u_time + 6.2 * offset);
+
+    //         vec2 pos = neighbor + offset - f_st;
+
+    //         float dist = length(pos);
+
+    //         m_dist = min(m_dist, m_dist * dist);
+    //     }
+    // }
+
+    // graphic += step(0.060, m_dist);
+
+    // -----產生一個密度紋理的網格坐標??--------------------------------------------- //
+
+    vec2 uv_texture = fract(floor(u_density_texture) * uv);//?
+    //將 u_density_texture 中的值向下取整，得到最接近且小於 u_density_texture 的整數值。這將產生一個密度紋理的網格坐標。
+    //fract(...) * uv：將上一步得到的網格坐標進行 fract 函數運算，得到該網格內的相對坐標（小數部分）。然後，將這個相對坐標與原始的紋理座標 uv 相乘。
+    
+    //------------rgb色板取樣向關?-------------
+    float shading;
+
+    if (u_rgb.r == 1.0) { shading += texture2D(u_texture_base, fract(floor(u_density_picture) * uv)).r; }
+    if (u_rgb.g == 1.0) { shading += texture2D(u_texture_base, fract(floor(u_density_picture) * uv)).g; }
+    if (u_rgb.b == 1.0) { shading += texture2D(u_texture_base, fract(floor(u_density_picture) * uv)).b; }
+
+    // if (u_rgb.r == 1.0) { shading += graphic.r; }
+    // if (u_rgb.g == 1.0) { shading += graphic.g; }
+    // if (u_rgb.b == 1.0) { shading += graphic.b; }
+    
+    if (u_rgb.r + u_rgb.g + u_rgb.b >= 1.0) { shading /= u_rgb.r + u_rgb.g + u_rgb.b; }
+    else { shading = 1.0; }
+    //歸一化：如果 u_rgb 中的任何一個通道為1.0，即任何通道影響了 shading，那麼將 shading 除以所有通道的總和，以實現歸一化。
+    //如果所有通道的總和小於1.0，則將 shading 設為1.0。這樣的操作可能是為了確保 shading 的值處於一個合理的範圍內，並受到各通道的影響。
+    //---------------老師的模板--------------
+    vec4 texture;
+
+    float step = 1.0 / 6.0;
+
+    if (shading <= step)
+    {
+        texture = texture2D(u_texture_5, uv_texture);
+    }
+    if (shading > step && shading <= 2.0 * step)
+    {
+        texture = texture2D(u_texture_4, uv_texture);
+    }
+    if (shading > 2.0 * step && shading <= 3.0 * step)
+    {
+        texture = texture2D(u_texture_3, uv_texture);
+    }
+    if (shading > 3.0 * step && shading <= 4.0 * step)
+    {
+        texture = texture2D(u_texture_2, uv_texture);
+    }
+    if (shading > 4.0 * step && shading <= 5.0 * step)
+    {
+        texture = texture2D(u_texture_1, uv_texture);
+    }
+    if (shading > 5.0 * step)
+    {
+        texture = texture2D(u_texture_0, uv_texture);
+    }
+
+    // -------------------------------------------------- //
+
+//滑鼠附近產生顏色過渡。
+//根據 texture_mouse 值產生一插值效果
+    vec4 color_ink = vec4(colorTransform(u_color_ink), 1.0);//正規畫??
+    vec4 color_background = vec4(colorTransform(u_color_background), 1.0);//正規畫??
+
+    vec4 shader_mouse = mix(mix(color_background, color_ink, texture), 
+                            mix(color_ink, color_background, texture),
+                            texture_mouse);//?應該是條顏色
+
+    // vec4 shader_title = mix(mix(color_ink, color_background, texture), 
+    //                         mix(color_background, color_ink, texture),
+    //                         texture_title);
+
+    // vec4 shader_guide = mix(mix(color_ink, color_background, texture), 
+    //                         mix(color_background, color_ink, texture),
+    //                         texture_guide);
+
+    vec4 shader_title = mix(vec4(0.0, 0.0, 0.0, 0.0), 
+                            color_ink,
+                            texture_title);
+
+    vec4 shader_guide = mix(vec4(0.0, 0.0, 0.0, 0.0), 
+                            color_ink,
+                            texture_guide);
+
+    vec4 shader_intro_background = mix(vec4(0.0, 0.0, 0.0, 0.0),
+                                       color_ink,
+                                       texture_intro_background);
+
+    vec4 shader_intro_title = mix(vec4(0.0, 0.0, 0.0, 0.0),
+                                       color_background,
+                                       texture_intro_title);
+
+    vec4 shader_intro_content = mix(vec4(0.0, 0.0, 0.0, 0.0),
+                                    color_background,
+                                    texture_intro_content);
+
+//進去的畫面
+    vec4 shader = mix(shader_mouse, shader_title, texture_title);//疊合滑鼠跟背景?
+    shader = mix(shader, shader_guide, texture_guide);//呈上，疊合
+//進去+被cursor的畫面
+    vec4 shader_intro = mix(shader, shader_intro_background, texture_intro_background);
+    shader_intro = mix(shader_intro, shader_intro_title, texture_intro_title);
+    shader_intro = mix(shader_intro, shader_intro_content, texture_intro_content);
+//滑鼠位子互動
+    if (uv_mouse.x <= 0.1) { gl_FragColor = shader_intro; }
+    else { gl_FragColor = shader; }
+}
---
+++ mySketch.js
@@ -0,0 +1,352 @@
+const PIXEL_DENSITY = 2;
+let theShader;
+let canvas;
+let capture;
+
+let f;
+let title;
+let guide;
+
+let intro_background;
+let intro_title;
+let intro_content;
+
+let texture_base;
+// let texture_A0; let texture_A1; let texture_A2; let texture_A3; let texture_A4; let texture_A5;
+// let texture_B0; let texture_B1; let texture_B2; let texture_B3; let texture_B4; let texture_B5;
+let texture_C0; let texture_C1; let texture_C2; let texture_C3; let texture_C4; let texture_C5;
+// let texture_D0; let texture_D1; let texture_D2; let texture_D3; let texture_D4; let texture_D5;
+let texture_E0; let texture_E1; let texture_E2; let texture_E3; let texture_E4; let texture_E5;
+let texture_F0; let texture_F1; let texture_F2; let texture_F3; let texture_F4; let texture_F5;
+
+
+let control = { //稍後會用來當作template控制元件
+	density_texture: 20.0,
+	density_picture: 1.0,
+
+	speed_x: 0.0,
+	speed_y: 0.05,
+	still: false,
+
+	r: true,
+	g: false,
+	b: false,
+
+	color_ink:[0, 0, 0], //[99, 125, 117],
+	color_backgroung: [255,255,255,],//[247, 222, 212],
+}
+
+var dropDown_style = {
+	selectedOption: 'No.1', //預設選項?
+	options: [
+		// '1. Grid',
+		// '2. Cross',
+		'No.1',
+		// '4. Pencil',
+		'No.2',
+		'No.3',//新增選項要在這!
+	]
+};
+
+//set GUI
+window.onload = function() {
+	var gui = new dat.GUI();
+	gui.domElement.id = 'gui';
+
+	// let gui_density = gui.addFolder("Density");
+	// gui_density.open();
+	// gui_density.add(control, 'density_texture', 1, 20).name("Texture").step(1);
+	// gui_density.add(control, 'density_picture', 1, 10).name("Picture").step(1);
+
+	let gui_speed = gui.addFolder("Speed");
+	gui_speed.open();
+	// gui_speed.add(control, 'speed_x', -0.2, 0.2).name("X-axis").step(0.01);
+	gui_speed.add(control, 'speed_y', -0.2, 0.2).name("Y-axis").step(0.01);
+	// gui_speed.add(control, 'still').name("Still");
+	
+	let gui_rgb = gui.addFolder("RGB");
+	gui_rgb.open();
+	gui_rgb.add(control, 'r').name("R");
+	gui_rgb.add(control, 'g').name("G");
+	gui_rgb.add(control, 'b').name("B");
+
+	// let gui_color = gui.addFolder("Color");
+	// gui_color.open();
+	// gui_color.addColor(control, 'color_ink').name("Ink");
+	// gui_color.addColor(control, 'color_backgroung').name("Backgroung");
+
+	gui.add(dropDown_style, 'selectedOption', dropDown_style.options).name("Style");
+};
+
+// -只須看這邊------------------- preload -------------------- //
+
+function preload(){
+	theShader = loadShader('vert.glsl', 'May.frag');//改成自己的
+
+	// texture_base = loadImage("data/TheStarryNight.jpg");
+	texture_base = loadImage("data/test_tem1.jpg");
+
+	// texture_A0 = loadImage("data/1_grid/hatch_0.jpg");
+	// texture_A1 = loadImage("data/1_grid/hatch_1.jpg");
+	// texture_A2 = loadImage("data/1_grid/hatch_2.jpg");
+	// texture_A3 = loadImage("data/1_grid/hatch_3.jpg");
+	// texture_A4 = loadImage("data/1_grid/hatch_4.jpg");
+	// texture_A5 = loadImage("data/1_grid/hatch_5.jpg");
+
+	// texture_B0 = loadImage("data/2_cross/hatch_0.jpg");
+	// texture_B1 = loadImage("data/2_cross/hatch_1.jpg");
+	// texture_B2 = loadImage("data/2_cross/hatch_2.jpg");
+	// texture_B3 = loadImage("data/2_cross/hatch_3.jpg");
+	// texture_B4 = loadImage("data/2_cross/hatch_4.jpg");
+	// texture_B5 = loadImage("data/2_cross/hatch_5.jpg");
+
+	texture_C0 = loadImage("data/3_circle/N0.jpg");
+	texture_C1 = loadImage("data/3_circle/N1.jpg");
+	texture_C2 = loadImage("data/3_circle/N2.jpg");
+	texture_C3 = loadImage("data/3_circle/N3.jpg");
+	texture_C4 = loadImage("data/3_circle/N4.jpg");
+	texture_C5 = loadImage("data/3_circle/N5.jpg");
+
+	// texture_D0 = loadImage("data/4_pencil/hatch_0.jpg");
+	// texture_D1 = loadImage("data/4_pencil/hatch_1.jpg");
+	// texture_D2 = loadImage("data/4_pencil/hatch_2.jpg");
+	// texture_D3 = loadImage("data/4_pencil/hatch_3.jpg");
+	// texture_D4 = loadImage("data/4_pencil/hatch_4.jpg");
+	// texture_D5 = loadImage("data/4_pencil/hatch_5.jpg");
+
+	texture_E0 = loadImage("data/5_crayon/3D6-02.jpg");
+	texture_E1 = loadImage("data/5_crayon/3D5-02.jpg");
+	texture_E2 = loadImage("data/5_crayon/3D4-02.jpg");
+	texture_E3 = loadImage("data/5_crayon/3D3-02.jpg");
+	texture_E4 = loadImage("data/5_crayon/3D2-02.jpg");
+	texture_E5 = loadImage("data/5_crayon/3D1-02.jpg");
+
+	texture_F0 = loadImage("data/6_marker/C1.png");
+	texture_F1 = loadImage("data/6_marker/C2.png");
+	texture_F2 = loadImage("data/6_marker/C3.png");
+	texture_F3 = loadImage("data/6_marker/C4.png");
+	texture_F4 = loadImage("data/6_marker/C5.png");
+	texture_F5 = loadImage("data/6_marker/C6.png");
+
+	f = loadFont("https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Bold.otf");
+	// f = loadFont("https://www.fontsaddict.com/fontface/the-fiveonetwo.otf");
+	// f = loadFont("C:\Users\lym95\Downloads\the-fiveonetwo.ttf");
+}
+// @font-face {
+// 	font-family: 'The FiveOneTwo';
+// 	src: url('https://www.fontsaddict.com/fontface/the-fiveonetwo.ttf');
+//   }
+
+// -------------------- setup -------------------- //
+
+function setup() {
+	pixelDensity(PIXEL_DENSITY);
+	// canvas = createCanvas(1000,1000, WEBGL);
+	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+
+	// capture = createCapture(VIDEO);
+  	// capture.hide();
+
+	flipImage(texture_base);
+	// flipImage(texture_A0); flipImage(texture_A1); flipImage(texture_A2); flipImage(texture_A3); flipImage(texture_A4); flipImage(texture_A5);
+	// flipImage(texture_B0); flipImage(texture_B1); flipImage(texture_B2); flipImage(texture_B3); flipImage(texture_B4); flipImage(texture_B5);
+	flipImage(texture_C0); flipImage(texture_C1); flipImage(texture_C2); flipImage(texture_C3); flipImage(texture_C4); flipImage(texture_C5);
+	// flipImage(texture_D0); flipImage(texture_D1); flipImage(texture_D2); flipImage(texture_D3); flipImage(texture_D4); flipImage(texture_D5);
+	flipImage(texture_E0); flipImage(texture_E1); flipImage(texture_E2); flipImage(texture_E3); flipImage(texture_E4); flipImage(texture_E5);
+	flipImage(texture_F0); flipImage(texture_F1); flipImage(texture_F2); flipImage(texture_F3); flipImage(texture_F4); flipImage(texture_F5); 
+//!!!!這裡有褲東西
+	noCursor();
+	background(0);
+	noStroke();
+	shader(theShader);
+//作品介紹區，create graphic都是p5.js的渲染引擎，全部都去找p5的reference
+	let width_fixed = 1024;//why數字越大框框越窄?
+
+	title = createGraphics(width_fixed, windowHeight, WEBGL);
+	title.pixelDensity(PIXEL_DENSITY);
+	title.textFont(f);
+	title.textSize(120);
+	title.textAlign(LEFT, BASELINE);
+	title.fill(color(255, 255, 255));
+	title.text("HW", 0, 0);//後面兩個是移動位子!!!但單位不知道是誰麼好像是跟著textalign一起的
+
+	guide = createGraphics(width_fixed, windowHeight, WEBGL);
+	guide.pixelDensity(PIXEL_DENSITY);
+	guide.textFont(f);
+	guide.textSize(18);
+	guide.textAlign(LEFT, BASELINE);
+	guide.fill(color(255, 255, 255));
+	guide.text("<< cursor here to see intro", 0, 0);
+
+	intro_background = createGraphics(width_fixed, windowHeight, WEBGL);
+	intro_background.pixelDensity(PIXEL_DENSITY);
+	intro_background.fill(color(255, 255, 255));
+	intro_background.rect(width_fixed * -0.5, height * -0.5, 500, height);//相對於圖形物件中心的偏移。矩形的寬度是 500，高度是 height。
+	intro_background.circle(50,50,500);
+
+	intro_title = createGraphics(width_fixed, windowHeight, WEBGL);
+	intro_title.pixelDensity(PIXEL_DENSITY);//默认像素密度为显示器的像素密度，可调用 pixelDensity(1) 以关闭此功能。调用 pixelDensity() 并不给予任何参数将返回该绘图的像素密度。
+	intro_title.textFont(f);
+	intro_title.textSize(120);
+	intro_title.textAlign(LEFT, BASELINE);
+	intro_title.fill(color(255, 255, 255));//先設白色才不會被影響
+	intro_title.text("HW02", 0, 0);
+
+	intro_content = createGraphics(width_fixed, windowHeight, WEBGL);
+	intro_content.pixelDensity(PIXEL_DENSITY);
+	intro_content.textFont(f);
+	intro_content.textSize(18);
+	intro_content.textAlign(LEFT, BASELINE);
+	intro_content.fill(color(255, 255, 255));
+	intro_content.textWrap(WORD);
+	intro_content.text("[ Computational Aesthetics ]\n[ Work by Leo Kao ]\n\nThis work demonstrates different hatching texture.\nYou can play with this shader by adjusting variables on the right side.\n\n:)", 0, 0, 450);
+}
+
+// PASS variable!-------------------- draw -------------------- //
+
+function draw() {
+	// flipVideo(capture);
+	// flipImage(capture);
+
+	var y = (mouseY - 500) / min(1, windowWidth / windowHeight) + 1500;
+	
+	theShader.setUniform("u_resolution", [width * PIXEL_DENSITY, height * PIXEL_DENSITY]);
+	theShader.setUniform("u_mouse", [mouseX * PIXEL_DENSITY, (height-y) * PIXEL_DENSITY]);
+  	theShader.setUniform("u_time", millis() / 1000.0);
+
+	theShader.setUniform("u_density_texture", control.density_texture);
+	theShader.setUniform("u_density_picture", control.density_picture);
+
+	theShader.setUniform("u_speed_x", control.speed_x);
+	theShader.setUniform("u_speed_y", control.speed_y);
+	theShader.setUniform("u_still", control.still);
+	theShader.setUniform("u_mouse_pressed", mouseIsPressed);
+
+	theShader.setUniform("u_rgb", [control.r, control.g, control.b]);
+
+	theShader.setUniform("u_color_ink", control.color_ink);
+	theShader.setUniform("u_color_background", control.color_backgroung);
+	
+	theShader.setUniform("u_texture_base", texture_base);
+	// theShader.setUniform("u_texture_base", capture);
+
+	// if (dropDown_style.selectedOption == '1. Grid') {
+	// 	theShader.setUniform("u_texture_0", texture_A0);
+	// 	theShader.setUniform("u_texture_1", texture_A1);
+	// 	theShader.setUniform("u_texture_2", texture_A2);
+	// 	theShader.setUniform("u_texture_3", texture_A3);
+	// 	theShader.setUniform("u_texture_4", texture_A4);
+	// 	theShader.setUniform("u_texture_5", texture_A5);
+	// }
+	// else if (dropDown_style.selectedOption == '2. Cross') {
+	// 	theShader.setUniform("u_texture_0", texture_B0);
+	// 	theShader.setUniform("u_texture_1", texture_B1);
+	// 	theShader.setUniform("u_texture_2", texture_B2);
+	// 	theShader.setUniform("u_texture_3", texture_B3);
+	// 	theShader.setUniform("u_texture_4", texture_B4);
+	// 	theShader.setUniform("u_texture_5", texture_B5);
+	// }
+	if (dropDown_style.selectedOption == 'No.2') {
+		theShader.setUniform("u_texture_0", texture_C0);
+		theShader.setUniform("u_texture_1", texture_C1);
+		theShader.setUniform("u_texture_2", texture_C2);
+		theShader.setUniform("u_texture_3", texture_C3);
+		theShader.setUniform("u_texture_4", texture_C4);
+		theShader.setUniform("u_texture_5", texture_C5);
+	}
+	// else if (dropDown_style.selectedOption == '4. Pencil') {
+	// 	theShader.setUniform("u_texture_0", texture_D0);
+	// 	theShader.setUniform("u_texture_1", texture_D1);
+	// 	theShader.setUniform("u_texture_2", texture_D2);
+	// 	theShader.setUniform("u_texture_3", texture_D3);
+	// 	theShader.setUniform("u_texture_4", texture_D4);
+	// 	theShader.setUniform("u_texture_5", texture_D5);
+	// }
+	else if(dropDown_style.selectedOption == 'No.3') {
+		theShader.setUniform("u_texture_0", texture_E0);
+		theShader.setUniform("u_texture_1", texture_E1);
+		theShader.setUniform("u_texture_2", texture_E2);
+		theShader.setUniform("u_texture_3", texture_E3);
+		theShader.setUniform("u_texture_4", texture_E4);
+		theShader.setUniform("u_texture_5", texture_E5);
+	}
+	else if(dropDown_style.selectedOption == 'No.1') {
+		theShader.setUniform("u_texture_0", texture_F0);
+		theShader.setUniform("u_texture_1", texture_F1);
+		theShader.setUniform("u_texture_2", texture_F2);
+		theShader.setUniform("u_texture_3", texture_F3);
+		theShader.setUniform("u_texture_4", texture_F4);
+		theShader.setUniform("u_texture_5", texture_F5);
+	}
+
+	theShader.setUniform("u_title", title);
+	theShader.setUniform("u_guide", guide);
+
+	theShader.setUniform("u_intro_background", intro_background);
+	theShader.setUniform("u_intro_title", intro_title);
+	theShader.setUniform("u_intro_content", intro_content);
+	
+	rect(width * -0.5, height * -0.5, width, height);
+}
+
+// -------------------- windowResized -------------------- //
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+// -------------------- keyPressed -------------------- //
+
+function keyPressed() {
+	if (keyCode == ESCAPE) { dat.GUI.toggleHide(); }
+}
+
+// -------------------- flipImage -------------------- //
+
+function flipImage(img) {
+	img.loadPixels();
+
+	let w = img.width;
+	let h = img.height;
+
+	let flippedPixels = new Uint8Array(4 * w * h);
+  
+	for (let y = 0; y < h; y++) {
+	  for (let x = 0; x < w; x++) {
+		let index = (y * w + x) * 4;
+		let flippedIndex = ((h - y - 1) * w + x) * 4;
+
+		flippedPixels[flippedIndex] = img.pixels[index]; // Red
+		flippedPixels[flippedIndex + 1] = img.pixels[index + 1]; // Green
+		flippedPixels[flippedIndex + 2] = img.pixels[index + 2]; // Blue
+		flippedPixels[flippedIndex + 3] = img.pixels[index + 3]; // Alpha
+	  }
+	}
+  
+	img.pixels.set(flippedPixels);
+	img.updatePixels();
+}
+
+// -------------------- flipVideo -------------------- //
+
+function flipVideo(video) {
+	video.loadPixels();
+  
+	let flippedPixels = new Uint8Array(video.pixels);
+  
+	for (let y = 0; y < video.height; y++) {
+	  for (let x = 0; x < video.width; x++) {
+		let index = (x + y * video.width) * 4;
+  
+		let flippedIndex = ((video.width - 1 - x) + y * video.width) * 4;
+  
+		flippedPixels[flippedIndex] = video.pixels[index];
+		flippedPixels[flippedIndex + 1] = video.pixels[index + 1];
+		flippedPixels[flippedIndex + 2] = video.pixels[index + 2];
+		flippedPixels[flippedIndex + 3] = video.pixels[index + 3];
+	  }
+	}
+  
+	video.pixels.set(flippedPixels);
+	video.updatePixels();
+}
